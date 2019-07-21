6023d152f37ce5931fa5f219143d5b40cf49cea7
Beatriz <renderosb@gmail.com>
2019-07-20 22:57:34 -0400

    mongoose

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/BBCnews', { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI)
 
 
 app.get('/scrape', function(req, res) {
